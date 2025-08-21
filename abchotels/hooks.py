e48801845c260426dc701bf6964cbf8827f03822
darwishdev <a.darwish.dev@gmail.com>
2025-08-21 05:10:44 +0300

    initial doctypes

--- abchotels/hooks.py
+++ abchotels/hooks.py
@@ -1,244 +1,39 @@
+# -*- coding: utf-8 -*-
+from . import __version__ as app_version
+
 app_name = "abchotels"
-app_title = "Abchotels"
-app_publisher = "darwishdev"
-app_description = "Hotels Module"
-app_email = "a.darwish.dev@gmail.com"
-app_license = "mit"
+app_title = "ABC Hotels"
+app_publisher = "Your Name"
+app_description = "Custom app for hotel management"
+app_icon = "octicon octicon-home"
+app_color = "blue"
+app_email = "your.email@example.com"
+app_license = "MIT"
 
-# Apps
-# ------------------
+app_include_css = "/assets/abchotels/css/abchotels.css"
+app_include_js = "/assets/abchotels/js/abchotels.js"
 
-# required_apps = []
+desk_page = {"ABC Hotels": "abchotels/workspace/abc_hotels"}
 
-# Each item in the list will be shown as an app in the apps page
-# add_to_apps_screen = [
-# 	{
-# 		"name": "abchotels",
-# 		"logo": "/assets/abchotels/logo.png",
-# 		"title": "Abchotels",
-# 		"route": "/abchotels",
-# 		"has_permission": "abchotels.api.permission.has_app_permission"
-# 	}
-# ]
+website_include_js = ["/assets/abchotels/js/website.js"]
 
-# Includes in <head>
-# ------------------
+website_route_rules = [
+    {"from_route": "/abchotels/<path:app_path>", "to_route": "abchotels"}
+]
 
-# include js, css files in header of desk.html
-# app_include_css = "/assets/abchotels/css/abchotels.css"
-# app_include_js = "/assets/abchotels/js/abchotels.js"
-
-# include js, css files in header of web template
-# web_include_css = "/assets/abchotels/css/abchotels.css"
-# web_include_js = "/assets/abchotels/js/abchotels.js"
-
-# include custom scss in every website theme (without file extension ".scss")
-# website_theme_scss = "abchotels/public/scss/website"
-
-# include js, css files in header of web form
-# webform_include_js = {"doctype": "public/js/doctype.js"}
-# webform_include_css = {"doctype": "public/css/doctype.css"}
-
-# include js in page
-# page_js = {"page" : "public/js/file.js"}
-
-# include js in doctype views
-# doctype_js = {"doctype" : "public/js/doctype.js"}
-# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
-# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
-# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
-
-# Svg Icons
-# ------------------
-# include app icons in desk
-# app_include_icons = "abchotels/public/icons.svg"
-
-# Home Pages
-# ----------
-
-# application home page (will override Website Settings)
-# home_page = "login"
-
-# website user home page (by Role)
-# role_home_page = {
-# 	"Role": "home_page"
-# }
-
-# Generators
-# ----------
-
-# automatically create page for each record of this doctype
-# website_generators = ["Web Page"]
-
-# Jinja
-# ----------
-
-# add methods and filters to jinja environment
-# jinja = {
-# 	"methods": "abchotels.utils.jinja_methods",
-# 	"filters": "abchotels.utils.jinja_filters"
-# }
-
-# Installation
-# ------------
-
-# before_install = "abchotels.install.before_install"
-# after_install = "abchotels.install.after_install"
-
-# Uninstallation
-# ------------
-
-# before_uninstall = "abchotels.uninstall.before_uninstall"
-# after_uninstall = "abchotels.uninstall.after_uninstall"
-
-# Integration Setup
-# ------------------
-# To set up dependencies/integrations with other apps
-# Name of the app being installed is passed as an argument
-
-# before_app_install = "abchotels.utils.before_app_install"
-# after_app_install = "abchotels.utils.after_app_install"
-
-# Integration Cleanup
-# -------------------
-# To clean up dependencies/integrations with other apps
-# Name of the app being uninstalled is passed as an argument
-
-# before_app_uninstall = "abchotels.utils.before_app_uninstall"
-# after_app_uninstall = "abchotels.utils.after_app_uninstall"
-
-# Desk Notifications
-# ------------------
-# See frappe.core.notifications.get_notification_config
-
-# notification_config = "abchotels.notifications.get_notification_config"
-
-# Permissions
-# -----------
-# Permissions evaluated in scripted ways
-
-# permission_query_conditions = {
-# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
-# }
-#
-# has_permission = {
-# 	"Event": "frappe.desk.doctype.event.event.has_permission",
-# }
-
-# DocType Class
-# ---------------
-# Override standard doctype classes
-
-# override_doctype_class = {
-# 	"ToDo": "custom_app.overrides.CustomToDo"
-# }
-
-# Document Events
-# ---------------
-# Hook on document methods and events
-
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-# 	}
-# }
-
-# Scheduled Tasks
-# ---------------
-
-# scheduler_events = {
-# 	"all": [
-# 		"abchotels.tasks.all"
-# 	],
-# 	"daily": [
-# 		"abchotels.tasks.daily"
-# 	],
-# 	"hourly": [
-# 		"abchotels.tasks.hourly"
-# 	],
-# 	"weekly": [
-# 		"abchotels.tasks.weekly"
-# 	],
-# 	"monthly": [
-# 		"abchotels.tasks.monthly"
-# 	],
-# }
-
-# Testing
-# -------
-
-# before_tests = "abchotels.install.before_tests"
-
-# Overriding Methods
-# ------------------------------
-#
-# override_whitelisted_methods = {
-# 	"frappe.desk.doctype.event.event.get_events": "abchotels.event.get_events"
-# }
-#
-# each overriding function accepts a `data` argument;
-# generated from the base implementation of the doctype dashboard,
-# along with any modifications made in other Frappe apps
-# override_doctype_dashboards = {
-# 	"Task": "abchotels.task.get_dashboard_data"
-# }
-
-# exempt linked doctypes from being automatically cancelled
-#
-# auto_cancel_exempted_doctypes = ["Auto Repeat"]
-
-# Ignore links to specified DocTypes when deleting documents
-# -----------------------------------------------------------
-
-# ignore_links_on_delete = ["Communication", "ToDo"]
-
-# Request Events
-# ----------------
-# before_request = ["abchotels.utils.before_request"]
-# after_request = ["abchotels.utils.after_request"]
-
-# Job Events
-# ----------
-# before_job = ["abchotels.utils.before_job"]
-# after_job = ["abchotels.utils.after_job"]
-
-# User Data Protection
-# --------------------
-
-# user_data_fields = [
-# 	{
-# 		"doctype": "{doctype_1}",
-# 		"filter_by": "{filter_by}",
-# 		"redact_fields": ["{field_1}", "{field_2}"],
-# 		"partial": 1,
-# 	},
-# 	{
-# 		"doctype": "{doctype_2}",
-# 		"filter_by": "{filter_by}",
-# 		"partial": 1,
-# 	},
-# 	{
-# 		"doctype": "{doctype_3}",
-# 		"strict": False,
-# 	},
-# 	{
-# 		"doctype": "{doctype_4}"
-# 	}
-# ]
-
-# Authentication and authorization
-# --------------------------------
-
-# auth_hooks = [
-# 	"abchotels.auth.validate"
-# ]
-
-# Automatically update python controller files with type annotations for this app.
-# export_python_type_annotations = True
-
-# default_log_clearing_doctypes = {
-# 	"Logging DocType Name": 30  # days to retain logs
-# }
-
+fixtures = [
+    "Bed Type",
+    "Property",
+    "Room Category",
+    "Room Type",
+    "Room Type Room",
+    "Room Type Room Bed",
+    # the main workspace doc(s)
+    {"doctype": "Workspace", "filters": [["module", "in", ["ABC Hotels"]]]},
+    # children / related items shown on the workspace
+    {"doctype": "Workspace Link", "filters": [["parent", "in", ["ABC Hotels"]]]},
+    {"doctype": "Workspace Shortcut", "filters": [["parent", "in", ["ABC Hotels"]]]},
+    {"doctype": "Workspace Permission", "filters": [["parent", "in", ["ABC Hotels"]]]},
+    # include cards if you used them
+    {"doctype": "Number Card", "filters": [["module", "in", ["ABC Hotels"]]]},
+]
